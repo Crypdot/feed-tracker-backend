27284cc79d221b2f545a4ca3c981af0166b5a3ff
Alex <alexandersanmig@gmail.com>
2023-08-24 14:01:41 +0300

    Further tidying up. Updated to do.

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-/** To do */
-
 const express = require('express');
 const mongoose = require('mongoose');
 mongoose.set('debug', true);
@@ -20,7 +18,6 @@ const conn = mongoose.connect('mongodb://127.0.0.1:27017/feed-tracker', {
 })
     .then(() => console.log('Connected to the database successfully!'))
     .catch(console.error());
-
 
 /** Pet related requests */
 
